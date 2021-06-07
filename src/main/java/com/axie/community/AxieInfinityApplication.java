d6eb89c7ecd85d5403619a97ba07a7c041741e9a
SalahEddine <aityoussef.salaheddine@gmail.com>
2021-06-07 21:23:27 +0200

    first commit

---
+++ src/main/java/com/axie/community/AxieInfinityApplication.java
@@ -0,0 +1,13 @@
+package com.axie.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxieInfinityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxieInfinityApplication.class, args);
+    }
+
+}
